5aabaa506af91de59bf2176ebb74cb4acf6bb468
KACHIBHOTLA NAGA SAI SIRIAKSHAYA <knsakki@gmail.com>
2025-01-17 17:42:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import mysql.connector
 def get_db_connection():
     conn = mysql.connector.connect(
         host='localhost',
-        port='3306',
-        user='root',
-        password='root',
+        port='3306',#enter your MySQL port number here 
+        user='user_name',#enter your MySQL username here
+        password='password',#enter your MySQL password here
         database='student_management'
     )
     return conn
